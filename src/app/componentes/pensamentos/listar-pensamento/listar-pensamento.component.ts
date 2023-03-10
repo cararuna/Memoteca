6595cec6f545eed26c7b3af5877f83f84aef8fed
Caio <cararuna@gmail.com>
2023-03-10 15:11:42 -0300

    feat: :sparkles: recarrega componente para mostrar o mural
    Signed-off-by: Caio <cararuna@gmail.com>

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,6 +1,7 @@
 import { PensamentoService } from './../pensamento.service';
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -17,7 +18,7 @@ export class ListarPensamentoComponent implements OnInit {
   favoritos: boolean = false;
   listaFavoritos: Pensamento[] = [];
 
-  constructor(private service: PensamentoService) {}
+  constructor(private service: PensamentoService, private router: Router) {}
 
   ngOnInit(): void {
     this.service
@@ -35,6 +36,14 @@ export class ListarPensamentoComponent implements OnInit {
     if (!this.listaPensamentos.length) {
       this.haMaisPensamentos = false;
     }
+  }
+
+  recarregarComponente() {
+    this.favoritos = false;
+    this.paginaAtual = 1;
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([this.router.url]);
   }
 
   listarFavoritos() {
